8eb740ea82612772b7eb8dd43794792e11ce0b96
Oleksandr <alexundercover191@gmail.com>
2025-03-14 20:56:37 +0200

    Spin and rotate trophy cup

---
+++ Assets/_Scripts/TrophyGoldCup.cs
@@ -0,0 +1,14 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class TrophyGoldCup : MonoBehaviour
+{
+    [SerializeField] private float _cycleLength = 1.5f;
+    [SerializeField] private float _lenghtMultiplier = 10f;
+
+    void Start()
+    {
+        transform.DOMoveY(1.5f, _cycleLength).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DORotate(new Vector3(0, 360, 0), _cycleLength * _lenghtMultiplier, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+    }
+}
