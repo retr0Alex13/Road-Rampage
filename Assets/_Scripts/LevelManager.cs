a1c373def0a114ff649c0f08034c74f0979a0f2c
Oleksandr <alexundercover191@gmail.com>
2025-03-22 20:35:20 +0200

    Fix level unlock bug

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -1,24 +1,26 @@
+using TMPro;
 using UnityEngine;
-
 namespace Voidwalker
 {
     public class LevelManager : MonoBehaviour
     {
         [SerializeField] private GameObject[] _levelButtons;
         [SerializeField] private GameObject[] _lockedLevelButtons;
-
         private const string LEVEL_PREFS_KEY = "UnlockedLevel_";
+        private const string CURRENT_LEVEL_KEY = "CurrentLevel_";
         private int _maxUnlockedLevel = 0;
+        private int _currentLevel = 0;
 
         private void Start()
         {
+            //PlayerPrefs.DeleteAll();
             _maxUnlockedLevel = GetLatestUnlockedLevel();
+            _currentLevel = PlayerPrefs.GetInt(CURRENT_LEVEL_KEY, 0);
 
             if (_maxUnlockedLevel == 0)
             {
                 UnlockLevel(0);
             }
-
             UpdateLevelButtons();
         }
 
@@ -29,27 +31,41 @@ namespace Voidwalker
 
         private void UnlockLevel(int levelIndex)
         {
-            _maxUnlockedLevel = levelIndex;
+            _maxUnlockedLevel = Mathf.Max(_maxUnlockedLevel, levelIndex);
             PlayerPrefs.SetInt(LEVEL_PREFS_KEY + "Max", _maxUnlockedLevel);
-
             UpdateLevelButtons();
-
             PlayerPrefs.Save();
         }
 
         private void UpdateLevelButtons()
         {
-            for (int i = 0; i <= _maxUnlockedLevel; i++)
+            for (int i = 0; i <= _maxUnlockedLevel && i < _levelButtons.Length; i++)
             {
                 _levelButtons[i].SetActive(true);
                 _lockedLevelButtons[i].SetActive(false);
+                _levelButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = (i + 1).ToString();
             }
+        }
+
+        public void SetCurrentLevel(int level)
+        {
+            _currentLevel = level;
+            PlayerPrefs.SetInt(CURRENT_LEVEL_KEY, _currentLevel);
+            PlayerPrefs.Save();
         }
 
         public void UnlockNextLevel()
         {
-            int currentLevel = GetLatestUnlockedLevel();
-            UnlockLevel(currentLevel + 1);
+            int nextLevel = _currentLevel + 1;
+            if (nextLevel < _levelButtons.Length)
+            {
+                UnlockLevel(nextLevel);
+            }
+        }
+
+        public int GetCurrentLevel()
+        {
+            return _currentLevel;
         }
     }
 }
